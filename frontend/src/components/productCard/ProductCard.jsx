9ae69c0285b2466468f0e32dd99c4fe811f51342
salilaherath <salilaherath@gmail.com>
2022-10-29 12:33:58 +0530

    New arrivals section done

--- frontend/src/components/productCard/ProductCard.jsx
+++ frontend/src/components/productCard/ProductCard.jsx
@@ -1,6 +1,9 @@
 import './productCard.scss';
 import { useState } from 'react';
 
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import StarIcon from '@mui/icons-material/Star';
+
 const ProductCard = () => {
   const [products, setProducts] = useState([
     { image: 'product1', name: 'Jogger Pant', price: '2350.00', id: 1 },
@@ -20,13 +23,19 @@ const ProductCard = () => {
           <div className='image'>
             <img src={require('../../assets/products/' + product.image + '.jpg')} alt='' />
           </div>
-          <div className='name'>
-            {product.name}
-            <span>H</span>
+          <div className='fRow'>
+            <div className='name'>{product.name}</div>
+            <ShoppingCartOutlinedIcon className='cart' />
           </div>
-          <div className='price'>
-            {product.price}
-            <span>* * * * *</span>
+
+          <div className='price'>Rs. {product.price}</div>
+
+          <div className='rate'>
+            <StarIcon className='star' />
+            <StarIcon className='star' />
+            <StarIcon className='star' />
+            <StarIcon className='star' />
+            <StarIcon className='star' />
           </div>
         </div>
       ))}
